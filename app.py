025c0fcca14f8861dee0f875a7c1507cc0d041eb
Rebeca Alvarez <rebeca_06@live.de>
2021-11-20 20:47:40 +0000

    yaml added

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 def home():
     return "Hello World, my name is Rebeca Alvarez Morales"
 
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
